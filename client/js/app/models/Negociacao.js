4cff740cc1baef6ea8f1e5c62e8c362cecd16264
André Kaoru Fuziyama <andre.fuziyama@enext.local>
2019-04-25 16:19:01 -0300

    Created Module Negociacao | JS

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,29 @@
+class Negociacao {
+
+    constructor(date, quantity, value) {
+
+        this._date = new Date(date.getTime());
+        this._quantity = quantity;
+        this._value = value;
+
+        // Returns de object in a frozen state - Shallow immutability
+        Object.freeze(this);
+        
+    }
+
+    get data() {
+        return new Date(this._date.getTime());
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    get volume() {
+        return this._value * this._quantity;
+    }
+}
